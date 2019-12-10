ac98357235b872d8485be2963314c862ff664af9
abdulshafiq0 <43822591+abdulshafiq0@users.noreply.github.com>
2019-12-10 22:27:39 +0000

    h
    h

--- PythonGUIGame.py
+++ PythonGUIGame.py
@@ -3,14 +3,14 @@ from guizero import *
 
 
 name ="";
-LogIn = App(title="Log In", width=300, height=175, layout="grid")
-LBUsernames = ListBox(LogIn,grid=[0,0])
 
-#message = Text(LogIn,text = "Login:",grid = [0,0])
-#utext = Text(LogIn,text="Username:",grid=[0,1])
-#TBusername = TextBox(LogIn,width=25,grid=[1,1])
-#ptext=Text(LogIn,text="Password:",grid=[0,2])                       
-#TBpassword = TextBox(LogIn,width=25,grid=[1,2])
+LogIn = App(title="Log In", layout="grid",width=400, height=415)
+LBUsernames = ListBox(LogIn,grid=[0,1],width = 30,height = 20)
+cya = Text(LogIn,text = "Choose Your \n Account:",grid = [0,0])
+TXTPassword= Text(LogIn,text="Password:",grid=[1,0])
+TBPassword =TextBox(LogIn,grid=[1,1],width = 25)
+
+
 
 NewUserF = App(title="NewUser", width=300, height=175, layout="grid")
 NewUserF.hide()
@@ -23,25 +23,34 @@ TBnewcpassowrd =TextBox(NewUserF,width=25,grid=[1,3])
 
 GameF = App(title=name, width=300, height=175, layout="grid")
 GameF.hide()
+
+Msgbox = App(title = "Message",height = 75)
+msg = Text(Msgbox,text = "",grid = [0,0])
+Msgbox.hide()
+
+def MsgBoxClose():
+    Msgbox.hide()
+
+def MessageBox(message):
+    msg.value = message
+    Msgbox.show()
 def GetUsernames():
     f = open("Users.txt","r")
     for line in f:
          arr = line.split("/")
-         LBUsernames.items.add(arr[0])
-         LogIn.Display()
+         LBUsernames.append(arr[0])
     f.close()
-        
+
 def CheckUser():
-    print("hi")
-    f = open("Users.txt","r")
-    for line in f:
-        if(line==TBusername.value+"/"+TBpassword.value):
-            name=TBusername.value
-            GameF.title = name;
-            print("Logged In")
-            LogIn.hide()
-            GameF.show()
-    f.close()
+    with open("Users.txt",mode="r") as f:
+        for line in f:
+            if(line == LBUsernames.value+"/"+TBPassword.value):
+                GameF.show();
+                GameF.title = LBUsernames.value
+                LogIn.hide()
+                return
+    MessageBox("Invalid Username or Password")
+
 def MakeNewUser():
     if(TBnewusername.value!=""and TBnewpassword.value !="" and TBnewcpassowrd.value!=""):
         if(TBnewpassword.value==TBnewcpassowrd.value):
@@ -49,25 +58,24 @@ def MakeNewUser():
                 for line in f:
                     arr = line.split("/")
                     if(arr[0]==TBnewusername.value):
-                        print("User already exists , please choose another one")
+                        MessageBox("User already exists , please choose another one")
                         return
             with open("Users.txt",mode="a+") as f:
                 f.writelines("\n"+TBnewusername.value+"/"+TBnewpassword.value)
             NewUserF.hide()
             LogIn.show()
-            print("Account Created")
+            MessageBox("Account Created")
             TBnewusername.value=""
             TBnewcpassowrd.value = ""
             TBnewpassword.value = ""
+            LBUsernames.clear()
+            GetUsernames()
         else:
-            print("Passwords dont match")
+            MessageBox("Passwords dont match")
     else:
-        print("Please dont leave values empty")
+        MessageBox("Please dont leave values empty")
 def NewUser():
     LogIn.hide()
-
-    TBpassword.value = ""
-    TBusername.value =""
     NewUserF.show()
 def BackToLogIN():
     LogIn.show();
@@ -76,13 +84,15 @@ def BackToLogIN():
     TBnewcpassowrd.value = ""
     TBnewpassword.value = ""
 
-BTNConfirm = PushButton(LogIn,command = CheckUser,text="Confirm",grid=[1,3])
+BTNClose = PushButton(Msgbox,command = MsgBoxClose,text="Ok",grid=[0,1])
+BTNConfirm = PushButton(LogIn,command = CheckUser,text="Confirm",grid=[1,4])
 BTNNewUser = PushButton(LogIn,command = NewUser,text="New User?",grid = [0,4])
 BTNnuConfirm = PushButton(NewUserF,command = MakeNewUser,text="Confirm",grid=[1,4])
 BTNBack = PushButton(NewUserF,command = BackToLogIN,text="Back To \n Log In",grid=[0,5])
 
+GetUsernames()
+
 GameF.display()
 
 LogIn.display()
-GetUsernames()
 
